74be67031735886a1caec84ad062f848a1897bb8
Gurjeet Singh <developer.gurjeet@gmail.com>
2016-06-30 17:03:08 +0530

    Block and Template created

---
+++ SocialLinks/Block/Catalog/Product/Filter.php
@@ -0,0 +1,11 @@
+<?php 
+namespace GSS\SocialLinks\Block\Catalog\Product;
+
+class Filter extends \Magento\Framework\View\Element\Template{
+	
+	public function __construct( \Magento\Framework\View\Element\Template\Context $context, array $data = []) {
+    	parent::__construct($context, $data);
+	}
+}
+
+
